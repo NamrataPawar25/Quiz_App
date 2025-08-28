14584ca33ce6b7cf558010489053bc2fff421f0b
NamrataPawar25 <namratapawar9793@gmail.com>
2025-08-28 13:08:49 +0530

    data

---
+++ src/data.js
@@ -0,0 +1,68 @@
+// src/data.js
+export const questions = [
+  {
+    id: 1,
+    question: "Which language is used in React?",
+    options: ["Python", "JavaScript", "C++", "Java"],
+    answer: "JavaScript",
+  },
+  {
+    id: 2,
+    question: "What does HTML stand for?",
+    options: [
+      "Hyperlinks and Text Markup Language",
+      "Hyper Text Markup Language",
+      "Home Tool Markup Language",
+      "Hyperlinks Text Making Language",
+    ],
+    answer: "Hyper Text Markup Language",
+  },
+  {
+    id: 3,
+    question: "Which CSS property is used to change text color?",
+    options: ["font-style", "text-color", "color", "background-color"],
+    answer: "color",
+  },
+  {
+    id: 4,
+    question: "Inside which HTML element do we put JavaScript code?",
+    options: ["<script>", "<js>", "<javascript>", "<code>"],
+    answer: "<script>",
+  },
+  {
+    id: 5,
+    question: "Which of the following is NOT a JavaScript data type?",
+    options: ["String", "Boolean", "Float", "Number"],
+    answer: "Float",
+  },
+  {
+    id: 6,
+    question: "Which symbol is used for single-line comments in JavaScript?",
+    options: ["<!-- -->", "//", "/* */", "#"],
+    answer: "//",
+  },
+  {
+    id: 7,
+    question: "Which keyword is used to declare a variable in JavaScript (ES6)?",
+    options: ["var", "let", "const", "All of the above"],
+    answer: "All of the above",
+  },
+  {
+    id: 8,
+    question: "Which operator is used to compare both value and type in JavaScript?",
+    options: ["==", "===", "!=", "="],
+    answer: "===",
+  },
+  {
+    id: 9,
+    question: "In CSS, which property controls the size of text?",
+    options: ["font-style", "text-size", "font-size", "size"],
+    answer: "font-size",
+  },
+  {
+    id: 10,
+    question: "Which company developed Java programming language?",
+    options: ["Microsoft", "Sun Microsystems", "Google", "IBM"],
+    answer: "Sun Microsystems",
+  },
+];
